261a22f36cf56048b863381778220255f88c664b
nosey-dewdrop <damummyphus@gmail.com>
2026-01-03 02:50:51 +0300

    antifragile

--- validParantheses.java
+++ validParantheses.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
-
+import java.util.Stack;
 public class validParantheses {
       
       public static boolean isValid(String s) {
-            int braceCount = 0;
-            int bracketCount = 0;
-            int curlyCount = 0;
+            Stack<Character> stack = new Stack<>();
 
-            char[] charArray = s.toCharArray();
+            for (char c : s.toCharArray()) {
+                  if (c == '(' || c == '{' || c == '[') {
+                        stack.push(c);
+                  } 
 
-            for(char c: charArray){
-                  if(c == '('){
-                        braceCount++;
-                  }
-                  if(c == '['){
-                        bracketCount++;
-                  }
-                  if(c == '{'){
-                        curlyCount++;
-                  }
+                  else {
+                        if (stack.isEmpty()) {
+                              return false;
+                        }
 
+                        char top = stack.pop();
+                        if ((c == ')' && top != '(') ||
+                            (c == '}' && top != '{') ||
+                            (c == ']' && top != '[')) {
+                              return false;
+                        }
+                  }
             }
-
+            return stack.isEmpty();
       }     
 
       public static void main (String[] args){
