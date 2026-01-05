c3506394f73b5ce744dbb4ad68062b7c8fff19f8
nosey-dewdrop <damummyphus@gmail.com>
2026-01-05 07:07:30 +0300

    missing numbe with cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class MissingNumber {
+public:
+      int missingNumber(vector<int>& nums) {
+            int n = nums.size();
+            int expected = n * (n + 1) / 2;
+
+            int actual = 0;
+            for (int i = 0; i < n; i++) {
+                  actual += nums[i];
+            }
+
+            return expected - actual;
+      }
+};
---
+++ TwoSum.cpp
@@ -0,0 +1,11 @@
+class TwoSum {
+      public:
+
+};
+
+// you must learn data types.
+// what is a vector,  hashtabe,
+// store the complement value. 
+/**
+ * 
+ */
